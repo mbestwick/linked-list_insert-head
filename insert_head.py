b58d760462b1e6a26c66f599507e17b86a05ce3c
vagrant <vagrant@vagrant.vm>
2017-06-16 03:47:55 +0000

    solved HackerRank's insert node at head of linked list challenge

---
+++ insert_head.py
@@ -0,0 +1,28 @@
+"""
+You’re given the pointer to the head node of a linked list and an integer to add
+to the list. Create a new node with the given integer, insert this node at the
+head of the linked list and return the new head node. The head pointer given may
+be null meaning that the initial list is empty.
+
+Input Format
+You have to complete the Node* Insert(Node* head, int data) method which takes
+two arguments - the head of the linked list and the integer to insert. You
+should NOT read any input from stdin/console.
+
+Output Format
+Insert the new node at the head and return the head of the updated linked list.
+Do NOT print anything to stdout/console.
+
+"""
+
+
+class Node(object):
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next = next_node
+
+
+def Insert(head, data):
+    new_head = Node(data, head)
+    return new_head
